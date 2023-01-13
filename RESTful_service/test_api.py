8f6cbc05ef98cc08307d512f0aef0996bf1e926e
howff <howff>
2023-01-13 14:22:59 +0000

    test_api.py - fixed to work with https (ignore certificate error) and to send a password

--- RESTful_service/test_api.py
+++ RESTful_service/test_api.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # Run some sample queries by calling the API and printing the JSON response
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context # disable certificate check for HTTPS
 import urllib.request
 
 port="8485" # azure VM or nsh-smi06
@@ -8,14 +10,16 @@ port="8000" # docker
 
 passphrase="aa06b3414d1ef012810cff0cfa1e459318ebcdf033af6044bdde7533566b2c23"
 
-url_prefix = 'http://localhost:${port}/api/search_anns/C0205076/?passphrase=${passphrase}'
-url_prefix = 'http://localhost:${port}/api/search_anns/x/?'
+url_prefix = f'https://localhost:{port}/api/search_anns/C0205076/?passphrase={passphrase}'
+url_prefix = f'https://localhost:{port}/api/search_anns/x/?'
 
 # Password?
-url = 'http://localhost:8000/api/need_passphrase/'
+url = f'https://localhost:{port}/api/need_passphrase/'
 print('Need password? %s' % urllib.request.urlopen(urllib.request.Request(url)).read().decode())
-#url = 'http://localhost:8485/api/check_phrase/${passphrase}/'
-#print(urllib.request.urlopen(urllib.request.Request(url)).read().decode())
+url = f'http://localhost:{port}/api/check_phrase/{passphrase}/'
+print('Check password? %s' % urllib.request.urlopen(urllib.request.Request(url)).read().decode())
+
+passquery = f'&passphrase={passphrase}'
 
 #url = url_prefix + '&' + 'j={"terms":[{"q":"C0205076","negation":"Any","experiencer":"Any","temporality":"Any"}],"filter":{"modalities":["MR","CT"],"start_date":"2020-02-02","end_date":"2021-09-07"},"returnFields":["SOPInstanceUID","StudyInstanceUID","ContentDate"]}'
 #url = url_prefix + '&' + 'j={"terms":[{"q":"C0205076","negation":"Any","experiencer":"Any","temporality":"Any"}],"filter":{"modalities":["MR","CT"],"start_date":"2020-02-02","end_date":"2021-09-07"}}'
@@ -45,7 +49,7 @@ queries = [
 for query in queries:
     print('======================================================================================')
     query = query.replace('\n',' ').replace('\r','')
-    url = url_prefix + '&j=' + query
+    url = url_prefix + '&j=' + query + passquery
     req = urllib.request.Request(url)
     resp = urllib.request.urlopen(req).read().decode()
     print('--------------\n%s\n-------------\n' % resp)
